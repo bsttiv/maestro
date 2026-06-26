4608670d911f65e35ed4cd52259aa368542f969f
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 14:20:42 -0400

    Feat: Added method signatures for ISessionManager (not definitive)

--- src/main/scala/com/github/bsttiv/maestro/application/ISessionManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/application/ISessionManager.scala
@@ -1,5 +1,7 @@
 package com.github.bsttiv.maestro.application
 
 trait ISessionManager {
-
+  def generateSessionToken(): Unit;
+  def revokeSessionToken(): Unit;
+  def verifySessionToken(): Unit;
 }
