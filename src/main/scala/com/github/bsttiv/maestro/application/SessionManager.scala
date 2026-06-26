8aeaf234af3efd86bd020d20be3dae8548c44ce7
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 14:25:36 -0400

    Fix: TYPO in SessionManager

--- src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
@@ -3,7 +3,7 @@ package com.github.bsttiv.maestro.application
 import com.github.bsttiv.maestro.infrastructure.{JWTManager, KeystoreManager}
 
 class SessionManager(private val repository: IRepository) extends ISessionManager {
-  private val jwtManager = new JWTManager;g
+  private val jwtManager = new JWTManager;
 
   override def generateSessionToken(): Unit = ???
 
