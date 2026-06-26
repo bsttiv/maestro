34412d26b53f4d65ed6203e52479d6ee1db7e768
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 18:24:25 -0400

    Feat: Added missing method to IKeystoreManager

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
@@ -1,5 +1,10 @@
 package com.github.bsttiv.maestro.infrastructure
 
+import java.security.{KeyPair, PrivateKey, PublicKey}
+
 trait IKeystoreManager {
   def changeKey(): Unit;
+  def getPrivateKey: PrivateKey;
+  def getPublicKey: PublicKey;
+  def getOldKeyPair: KeyPair;
 }
