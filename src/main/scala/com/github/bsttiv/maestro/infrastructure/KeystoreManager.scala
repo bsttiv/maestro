1a187d5f1abb98c807939a9b5dae77386c38c472
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 18:23:32 -0400

    Feat: Wrapped variables in AtomicReference and exposed keys

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
@@ -2,18 +2,21 @@ package com.github.bsttiv.maestro.infrastructure
 
 import com.github.bsttiv.maestro.infrastructure.io.KeyReader
 
-import java.security.{KeyFactory, KeyPair, KeyPairGenerator, PrivateKey, Signature}
+import java.security.{KeyFactory, KeyPair, KeyPairGenerator, PrivateKey, PublicKey, Signature}
+import java.util.concurrent.atomic.AtomicReference
 import scala.io.Source
 
 class KeystoreManager(private val reader: KeyReader) extends IKeystoreManager {
   // TODO: Save keys in Redis in case server gets restarted
   private val keyGenerator = KeyPairGenerator.getInstance("Ed25519")
-  private var currentKeyPair = keyGenerator.generateKeyPair();
-  private var oldKeyPair = currentKeyPair;
+  private val currentKeyPair = AtomicReference[KeyPair](keyGenerator.generateKeyPair());
+  private val oldKeyPair = AtomicReference[KeyPair](currentKeyPair);
   override def changeKey(): Unit = {
     val newPair = keyGenerator.generateKeyPair();
-    oldKeyPair = currentKeyPair;
-    currentKeyPair = newPair;
+    oldKeyPair.set(currentKeyPair);
+    currentKeyPair.set(newPair);
   };
-  def getPrivateKey: PrivateKey = currentKeyPair.getPrivate;
+  def getPrivateKey: PrivateKey = currentKeyPair.get().getPrivate;
+  def getPublicKey: PublicKey = currentKeyPair.get().getPublic;
+  def getOldKeyPair: KeyPair = oldKeyPair.get();
 }
