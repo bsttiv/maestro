e3965268da6e3764351a2da9e7f1f55072de894c
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 15:18:25 -0400

    Feat: Added methods to KeystoreManage and its trait

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
@@ -1,5 +1,7 @@
 package com.github.bsttiv.maestro.infrastructure
 
 trait IKeystoreManager {
-
+  def changeKey(): Unit;
+  def deleteOldKey(): Unit;
+  def verifySignature(signature:String): Boolean;
 }
--- src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
@@ -1,5 +1,9 @@
 package com.github.bsttiv.maestro.infrastructure
 
 class KeystoreManager extends IKeystoreManager {
+  override def changeKey(): Unit = ???;
 
+  override def deleteOldKey(): Unit = ???;
+  
+  override def verifySignature(signature: String): Boolean = ???;
 }
