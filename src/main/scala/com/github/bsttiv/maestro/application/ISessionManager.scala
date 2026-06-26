ae627727554a54e6f46e7e81987ddd626abe7173
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 14:14:48 -0400

    Feat: Added SessionManager class with its trait

---
+++ src/main/scala/com/github/bsttiv/maestro/application/ISessionManager.scala
@@ -0,0 +1,5 @@
+package com.github.bsttiv.maestro.application
+
+trait ISessionManager {
+
+}
---
+++ src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
@@ -0,0 +1,5 @@
+package com.github.bsttiv.maestro.application
+
+class SessionManager extends ISessionManager {
+
+}
