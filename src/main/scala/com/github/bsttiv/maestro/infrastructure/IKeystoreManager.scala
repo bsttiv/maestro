b0d9c6541d768d5352c0d7cf09d1bb3588f03148
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 17:18:10 -0400

    Refactor: Removed deleteOldKey method

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/IKeystoreManager.scala
@@ -2,6 +2,5 @@ package com.github.bsttiv.maestro.infrastructure
 
 trait IKeystoreManager {
   def changeKey(): Unit;
-  def deleteOldKey(): Unit;
   def verifySignature(signature:String): Boolean;
 }
