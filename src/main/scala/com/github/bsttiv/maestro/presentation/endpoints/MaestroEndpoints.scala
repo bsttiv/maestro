4beb32e85049a5e2c3049f3b110b0156a2e26d86
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 19:42:26 -0400

    Feat: Added MaestroEndpoints class

---
+++ src/main/scala/com/github/bsttiv/maestro/presentation/endpoints/MaestroEndpoints.scala
@@ -0,0 +1,5 @@
+package com.github.bsttiv.maestro.presentation.endpoints
+
+class MaestroEndpoints(private val mintEndpoint: IMintEndpoint, private val revokeEndpoint: IRevokeEndpoint, private val verifyEndpoint: IVerifyEndpoint){
+
+}
