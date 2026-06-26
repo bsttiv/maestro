33c728ea501e0fac1d6038c63cb099ed4ca69914
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 19:39:11 -0400

    Feat: Added RedisRepository implementation with RedisFuture

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/IRepository.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/IRepository.scala
@@ -1,7 +1,9 @@
 package com.github.bsttiv.maestro.infrastructure
 
+import io.lettuce.core.RedisFuture
+
 trait IRepository {
-  def searchRecord(uuid:String): Unit;
-  def saveRecord(uuid:String, time:Int): Unit;
-  def revokeRecord(uuid:String): Unit;
+  def searchRecord(uuid:String): RedisFuture[String];
+  def saveRecord(uuid:String, time:Int): RedisFuture[String];
+  def revokeRecord(uuid:String): RedisFuture[String];
 }
--- src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
@@ -3,7 +3,7 @@ package com.github.bsttiv.maestro.infrastructure
 import io.lettuce.core.api.StatefulRedisConnection
 import io.lettuce.core.api.async.RedisAsyncCommands
 import io.lettuce.core.api.sync.RedisCommands
-import io.lettuce.core.{RedisClient, RedisURI}
+import io.lettuce.core.{RedisClient, RedisFuture, RedisURI}
 
 class RedisRepository private () extends IRepository{
   private val uri: RedisURI = RedisURI("localhost", 6379)
@@ -11,11 +11,17 @@ class RedisRepository private () extends IRepository{
   private val connection: StatefulRedisConnection = client.connect();
   private val commands: RedisAsyncCommands = connection.async();
 
-  override def saveRecord(uuid: String, time: Int): Unit = ???;
+  override def saveRecord(uuid: String, time: Int): RedisFuture[String] = {
+    commands.setex(f"session:$uuid", time, "Active");
+  };
 
-  override def revokeRecord(uuid: String): Unit = ???;
+  override def revokeRecord(uuid: String): RedisFuture[String] = {
+    commands.set(f"session:$uuid", "Revoked");
+  };
 
-  override def searchRecord(uuid: String): Unit = ???;
+  override def searchRecord(uuid: String): RedisFuture[String] = {
+    commands.get(f"session:$uuid");
+  };
 }
 
 object RedisRepository {
