7ddfa0aa64639b946e4987cc4b5fe416d7700967
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 14:57:29 -0400

    Feat: Added Singleton Pattern to RedisRepository and Redis init

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
@@ -1,5 +1,18 @@
 package com.github.bsttiv.maestro.infrastructure
 
-class RedisRepository extends IRepository {
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.async.RedisAsyncCommands
+import io.lettuce.core.api.sync.RedisCommands
+import io.lettuce.core.{RedisClient, RedisURI}
 
+class RedisRepository private () extends IRepository{
+  private val uri: RedisURI = RedisURI("localhost", 6379)
+  private val client: RedisClient = RedisClient(uri);
+  private val connection: StatefulRedisConnection = client.connect();
+  private val commands: RedisAsyncCommands = connection.async();
+}
+
+object RedisRepository {
+  private lazy val instance: RedisRepository = new RedisRepository()
+  def getInstance(): RedisRepository = instance;
 }
