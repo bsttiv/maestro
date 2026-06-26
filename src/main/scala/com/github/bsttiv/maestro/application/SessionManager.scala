e3134518f995287e7406aca1a1f8a36fc25e6315
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-26 14:25:03 -0400

    Refactor: removed unnecesary dependency in SessionManager

--- src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
@@ -3,8 +3,7 @@ package com.github.bsttiv.maestro.application
 import com.github.bsttiv.maestro.infrastructure.{JWTManager, KeystoreManager}
 
 class SessionManager(private val repository: IRepository) extends ISessionManager {
-  private val jwtManager = new JWTManager;
-  private val keystoreManager = new KeystoreManager;
+  private val jwtManager = new JWTManager;g
 
   override def generateSessionToken(): Unit = ???
 
