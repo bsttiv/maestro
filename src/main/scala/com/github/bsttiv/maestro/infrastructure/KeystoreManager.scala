0fe41ee17107a7757bef6801664ac25f52053b01
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:39:36 -0400

    Fix: Fixed calls to currentKeyPair

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/KeystoreManager.scala
@@ -1,19 +1,17 @@
 package com.github.bsttiv.maestro.infrastructure
 
-import com.github.bsttiv.maestro.infrastructure.io.KeyReader
-
 import java.security.{KeyFactory, KeyPair, KeyPairGenerator, PrivateKey, PublicKey, Signature}
 import java.util.concurrent.atomic.AtomicReference
 import scala.io.Source
 
-class KeystoreManager(private val reader: KeyReader) extends IKeystoreManager {
+class KeystoreManager extends IKeystoreManager {
   // TODO: Save keys in Redis in case server gets restarted
   private val keyGenerator = KeyPairGenerator.getInstance("Ed25519")
   private val currentKeyPair = AtomicReference[KeyPair](keyGenerator.generateKeyPair());
-  private val oldKeyPair = AtomicReference[KeyPair](currentKeyPair);
+  private val oldKeyPair = AtomicReference[KeyPair](currentKeyPair.get());
   override def changeKey(): Unit = {
     val newPair = keyGenerator.generateKeyPair();
-    oldKeyPair.set(currentKeyPair);
+    oldKeyPair.set(currentKeyPair.get());
     currentKeyPair.set(newPair);
   };
   def getPrivateKey: PrivateKey = currentKeyPair.get().getPrivate;
