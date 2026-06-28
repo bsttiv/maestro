7105ab084c91d51e505cfcca1c0c3dea411554eb
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:36:24 -0400

    Refactor: Changed methods signatures

--- src/main/scala/com/github/bsttiv/maestro/application/ISessionManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/application/ISessionManager.scala
@@ -1,7 +1,12 @@
 package com.github.bsttiv.maestro.application
 
+import com.github.bsttiv.maestro.domain.TokenPair
+import com.github.bsttiv.maestro.domain.models.{MintSessionRequest, RevocationRequest, ValidationRequest, ValidationResponse}
+
+import scala.concurrent.Future
+
 trait ISessionManager {
-  def generateSessionToken(): Unit;
-  def revokeSessionToken(): Unit;
-  def verifySessionToken(): Unit;
+  def generateSessionToken(req: MintSessionRequest): Future[Either[String, TokenPair]];
+  def revokeSessionToken(req: RevocationRequest): Future[Either[String, Unit]];
+  def validateSessionToken(req: ValidationRequest): Future[Either[String, ValidationResponse]];
 }
--- src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/application/SessionManager.scala
@@ -1,13 +1,17 @@
 package com.github.bsttiv.maestro.application
 
-import com.github.bsttiv.maestro.infrastructure.{JWTManager, KeystoreManager}
+import com.github.bsttiv.maestro.domain.TokenPair
+import com.github.bsttiv.maestro.domain.models.{MintSessionRequest, RevocationRequest, ValidationRequest, ValidationResponse}
+import com.github.bsttiv.maestro.infrastructure.{IRepository, JWTManager, KeystoreManager}
+
+import scala.concurrent.Future
 
 class SessionManager(private val repository: IRepository) extends ISessionManager {
-  private val jwtManager = new JWTManager;
+  private val jwtManager = new JWTManager(new KeystoreManager);
 
-  override def generateSessionToken(): Unit = ???
+  override def generateSessionToken(req: MintSessionRequest): Future[Either[String, TokenPair]] = ???
 
-  override def revokeSessionToken(): Unit = ???
+  override def revokeSessionToken(req: RevocationRequest): Future[Either[String, Unit]] = ???
 
-  override def verifySessionToken(): Unit = ???
+  override def validateSessionToken(req: ValidationRequest): Future[Either[String, ValidationResponse]] = ???
 }
