d0c33236b12b2b62688645dfb00ff8a6a61f11cc
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 18:44:07 -0400

    Feat: Make RedisRepository receive optional host and port, aswell as returning scala futures

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/RedisRepository.scala
@@ -5,26 +5,24 @@ import io.lettuce.core.api.async.RedisAsyncCommands
 import io.lettuce.core.api.sync.RedisCommands
 import io.lettuce.core.{RedisClient, RedisFuture, RedisURI}
 
-class RedisRepository private () extends IRepository{
+import scala.jdk.FutureConverters.*
+import scala.concurrent.{ExecutionContext, Future}
+
+class RedisRepository (host: String = "localhost", port: Int = 6379) extends IRepository {
   private val uri: RedisURI = RedisURI.create("localhost", 6379);
   private val client: RedisClient = RedisClient.create(uri);
   private val connection: StatefulRedisConnection[String, String] = client.connect();
   private val commands: RedisAsyncCommands[String, String] = connection.async();
 
-  override def saveRecord(uuid: String, time: Int): RedisFuture[String] = {
-    commands.setex(f"session:$uuid", time, "Active");
+  override def saveRecord(uuid: String, time: Long): Future[String] = {
+    commands.setex(f"session:$uuid", time, "Active").asScala;
   };
 
-  override def revokeRecord(uuid: String): RedisFuture[String] = {
-    commands.set(f"session:$uuid", "Revoked");
+  override def revokeRecord(uuid: String): Future[String] = {
+    commands.set(f"session:$uuid", "Revoked").asScala;
   };
 
-  override def searchRecord(uuid: String): RedisFuture[String] = {
-    commands.get(f"session:$uuid");
+  override def searchRecord(implicit ec: ExecutionContext, uuid: String): Future[Option[String]] = {
+    commands.get(f"session:$uuid").asScala.map(v => Option(v));
   };
-}
-
-object RedisRepository {
-  private lazy val instance: RedisRepository = new RedisRepository()
-  def getInstance(): RedisRepository = instance;
 }
