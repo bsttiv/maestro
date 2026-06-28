2cab37e63dbd7ffa55f1e6c18323f2bbca134a1b
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 18:43:36 -0400

    Feat: New methods implementation

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/JWTManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/JWTManager.scala
@@ -1,20 +1,22 @@
 package com.github.bsttiv.maestro.infrastructure
 
-import com.github.bsttiv.maestro.domain.TokenClaims
+import com.github.bsttiv.maestro.domain.TokenClaimsAccess
 import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
+import io.circe.Encoder
 
 import java.security.PublicKey
+import java.time.Instant
 import io.circe.syntax.*
 import io.circe.parser.decode
 import scala.util.Try
 
 class JWTManager(private val keystoreManager: IKeystoreManager) extends IJWTManager {
 
-  override def verifySignature(token: String, publicKey: PublicKey): Boolean = {
-    JwtCirce.isValid(token, publicKey, Seq(JwtAlgorithm.EdDSA))
-  }
+  override def tokenFingerprint(token: TokenClaimsAccess, ctx: String): Boolean = token.ctx == ctx
 
-  override def generateToken(claims: TokenClaims): String = {
+  override def tokenExpired(token: TokenClaimsAccess): Boolean = Instant.now().getEpochSecond >= token.exp
+
+  override def generateToken[T: Encoder](claims: T): String = {
     val key = keystoreManager.getPrivateKey;
     val algo = JwtAlgorithm.EdDSA;
     val json = claims.asJson.noSpaces;
@@ -22,9 +24,9 @@ class JWTManager(private val keystoreManager: IKeystoreManager) extends IJWTMana
     JwtCirce.encode(jwtClaim, key, algo);
   }
 
-  override def decodeToken(token: String): Try[TokenClaims] = {
+  override def decodeToken(token: String): Try[TokenClaimsAccess] = {
     val key = keystoreManager.getPublicKey;
     val algo = JwtAlgorithm.EdDSA;
-    JwtCirce.decode(token, key, Seq(JwtAlgorithm.EdDSA)).flatMap(claim => decode[TokenClaims](claim.content).toTry);
+    JwtCirce.decode(token, key, Seq(JwtAlgorithm.EdDSA)).flatMap(claim => decode[TokenClaimsAccess](claim.content).toTry);
   }
 }
