1de87547f372754eaf1353ea478c21e64fd2dc1c
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:41:47 -0400

    Refactor: Changed verifyEndpoint to validateEndpoint

--- src/main/scala/com/github/bsttiv/maestro/presentation/endpoints/MaestroEndpoints.scala
+++ src/main/scala/com/github/bsttiv/maestro/presentation/endpoints/MaestroEndpoints.scala
@@ -1,5 +1,5 @@
 package com.github.bsttiv.maestro.presentation.endpoints
 
-class MaestroEndpoints(private val mintEndpoint: IMintEndpoint, private val revokeEndpoint: IRevokeEndpoint, private val verifyEndpoint: IVerifyEndpoint){
+class MaestroEndpoints(private val mintEndpoint: IMintEndpoint, private val revokeEndpoint: IRevokeEndpoint, private val validatemEndpoint: IValidateEndpoint){
 
 }
