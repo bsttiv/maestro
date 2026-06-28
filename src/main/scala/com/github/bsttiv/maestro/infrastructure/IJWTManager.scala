12c0547fd28323f3b152fcdcc88fa91b6f22e2e7
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:38:20 -0400

    Fix: Fixed method signature

--- src/main/scala/com/github/bsttiv/maestro/infrastructure/IJWTManager.scala
+++ src/main/scala/com/github/bsttiv/maestro/infrastructure/IJWTManager.scala
@@ -3,9 +3,10 @@ package com.github.bsttiv.maestro.infrastructure
 import com.github.bsttiv.maestro.domain.TokenClaims
 
 import java.security.PublicKey
+import scala.util.Try
 
 trait IJWTManager {
   def verifySignature(token: String, publicKey: PublicKey): Boolean;
   def generateToken(claims: TokenClaims): String;
-  def decodeToken(token: String): TokenClaims;
+  def decodeToken(token: String): Try[TokenClaims];
 }
