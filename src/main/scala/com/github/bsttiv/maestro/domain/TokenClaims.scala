7747330f26f140330cee0eaa4043563da41b68d0
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:37:46 -0400

    Feat: TokenClaims now derives Decoder

--- src/main/scala/com/github/bsttiv/maestro/domain/TokenClaims.scala
+++ src/main/scala/com/github/bsttiv/maestro/domain/TokenClaims.scala
@@ -1,5 +1,6 @@
 package com.github.bsttiv.maestro.domain
 
 import io.circe.Encoder
+import io.circe.Decoder
 
-case class TokenClaims(iss: String, sub: String, iat: int, expTime:String, jti: String, ctx: String) derives Encoder.AsObject;
+case class TokenClaims(iss: String, sub: String, iat: Int, expTime:String, jti: String, ctx: String) derives Encoder.AsObject, Decoder;
