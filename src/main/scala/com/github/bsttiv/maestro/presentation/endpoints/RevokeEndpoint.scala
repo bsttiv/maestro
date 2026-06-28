11ad9b0a02a620ef24586387458ce0fd6992b922
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 15:42:17 -0400

    Feat: RevokeEndpoint implementation

---
+++ src/main/scala/com/github/bsttiv/maestro/presentation/endpoints/RevokeEndpoint.scala
@@ -0,0 +1,30 @@
+package com.github.bsttiv.maestro.presentation.endpoints
+
+import com.github.bsttiv.maestro.application.ISessionManager
+import org.apache.pekko.http.javadsl.server.Route
+import com.github.bsttiv.maestro.domain.TokenPair
+import com.github.bsttiv.maestro.domain.models.RevocationRequest
+import sttp.tapir.*
+import sttp.tapir.json.circe.*
+import io.circe.generic.auto.*
+import org.apache.pekko.http.javadsl.server.directives.RouteAdapter
+import sttp.tapir.generic.auto.*
+import sttp.tapir.endpoint.EndpointType
+import sttp.tapir.server.pekkohttp.PekkoHttpServerInterpreter
+
+import scala.concurrent.ExecutionContext
+
+class RevokeEndpoint(private val baseEndpoint:EndpointType[Unit, Unit, Unit, Unit, Any], private val sessionManager: ISessionManager) extends IRevokeEndpoint {
+  private val revokeSessionEndpoint = baseEndpoint
+    .post
+    .in("revoke")
+    .in(jsonBody[RevocationRequest])
+    .out(statusCode(sttp.model.StatusCode.NoContent))
+    .errorOut(stringBody)
+    .description("Revokes a session in the database")
+  override def revokeRoute(using ec: ExecutionContext): Route = {
+    RouteAdapter.asJava(PekkoHttpServerInterpreter().toRoute(revokeSessionEndpoint.serverLogic(req => {
+      sessionManager.revokeSessionToken(req)
+    })))
+  }
+}
