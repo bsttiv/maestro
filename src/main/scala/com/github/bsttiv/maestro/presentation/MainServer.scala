a40c1dd6fc5df6687448f49947e9dfae1b44158a
bsttiv <bastian.valencia@ug.uchile.cl>
2026-06-28 18:45:41 -0400

    Feat: MainServer implementation

---
+++ src/main/scala/com/github/bsttiv/maestro/presentation/MainServer.scala
@@ -0,0 +1,41 @@
+package com.github.bsttiv.maestro.presentation
+
+import com.github.bsttiv.maestro.application.SessionManager
+import com.github.bsttiv.maestro.infrastructure.{JWTManager, KeystoreManager, RedisRepository}
+import com.github.bsttiv.maestro.presentation.endpoints.{MaestroEndpoints, MintEndpoint, RevokeEndpoint, ValidateEndpoint}
+import org.apache.pekko.actor.ActorSystem
+import sttp.tapir.*
+import org.apache.pekko.http.scaladsl.Http
+import sttp.tapir.endpoint
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.util.{Failure, Success}
+
+object MainServer {
+  def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem = ActorSystem("maestro-system")
+    implicit val ec: ExecutionContextExecutor = system.dispatcher
+    val redisRepository = new RedisRepository()
+    val keystoreManager = new KeystoreManager
+    val jwtManager = new JWTManager(keystoreManager)
+    val sessionManager = new SessionManager(redisRepository, jwtManager, ec)
+    val baseEndpoint = endpoint.in("api" / "v1" / "maestro")
+    val mintEndpoint = new MintEndpoint(baseEndpoint, sessionManager)
+    val revokeEndpoint = new RevokeEndpoint(baseEndpoint, sessionManager)
+    val validateEndpoint = new ValidateEndpoint(baseEndpoint, sessionManager)
+    val maestroEndpoints = MaestroEndpoints(mintEndpoint, revokeEndpoint, validateEndpoint)
+    val maestroRoutes = maestroEndpoints.routes(using ec)
+    val bindingFuture = Http(system).newServerAt("0.0.0.0", 8080).bind(maestroRoutes)
+    bindingFuture.onComplete {
+      case Success(binding) =>
+        println(s"Servidor Maestro Zero-Trust corriendo exitosamente en http://localhost:8080/")
+      case Failure(ex) =>
+        println(s"Error fatal al iniciar el servidor: ${ex.getMessage}")
+        system.terminate()
+    }
+
+
+    Await.result(system.whenTerminated, Duration.Inf)
+  }
+}
